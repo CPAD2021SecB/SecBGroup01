2ae5d738d15a6f8f78221fabe05b442039d39c9c
SutirthaSaha <sutirthasaha12@gmail.com>
2021-12-07 18:06:17 +0530

    sign-in page

--- netpix/lib/main.dart
+++ netpix/lib/main.dart
@@ -1,44 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:netpix/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Netpix',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'NetPix',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData
+        (
+        scaffoldBackgroundColor: Colors.white,
+        dialogBackgroundColor: Colors.white,
+        primarySwatch: Colors.grey,
+        cardColor: Colors.white70
       ),
-      home: const MyHomePage(title: "Home Page")
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text("Hello, World!")
-      ) // This trailing comma makes auto-formatting nicer for build methods.
+      home: const HomePage(),
     );
   }
 }
